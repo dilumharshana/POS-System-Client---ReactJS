d7eb5db43897a927229bb1258ff28f594df8cf49
dilumharshana <dilum.harshana123@gmail.com>
2021-09-03 01:32:02 +0530

    added password regex

--- src/configs/validationSchema.js
+++ src/configs/validationSchema.js
@@ -4,7 +4,13 @@ import * as yup from "yup";
 export const formHandler = (submit) => () => {
   const validationSchema = yup.object({
     email: yup.string().email("Invalid Email").required("Email required ."),
-    password: yup.string().required("password required"),
+    password: yup
+      .string()
+      .required("password required")
+      .matches(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/,
+        "Invalid password"
+      ),
   });
 
   return useFormik({
